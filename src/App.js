30b45f7e39c1bf6b09c89fc201df4378dbdbd4d2
Richard Saseun <richardsaseun@gmail.com>
2022-06-10 04:06:09 +0100

    chore: correct width

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LoginSlide from './components/loginSlide';
 function App() {
   return (
     <div className="md:flex h-screen">
-      <div className="px-5 md:px-10 md:w-5/12 h-full bg-[#2D74DA] pt-20">
+      <div className="px-5 md:px-10 md:w-6/12 h-full bg-[#2D74DA] pt-20">
         <p className="font-['Montserrat'] font-semibold text-[19.54px] tracking-[.21em] text-slate-50 mb-16">
           SME Better
         </p>
